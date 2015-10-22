546832adc9dd27a2d2db19dc5e6827015775a2be
Benny Daon <bennydaon@gmail.com>
2015-10-22 12:36:02 +0300

    Add a test for utf-8 data. it fails.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from gedcom import Gedcom
 from StringIO import StringIO
 
@@ -28,17 +29,11 @@ def test_header_only():
 
 def test_extranewline():
     g = Gedcom(stream="""0 HEAD\r
-1 SOUR FTW\r
-2 VERS 3.40\r
-2 NAME Family Tree Maker for Windows\r
-2 CORP Broderbund Software, Banner Blue Division\r
-3 ADDR 39500 Stevenson Pl.  #204\r
-4 CONT Fremont, CA 95439\r
-4 PHON (510) 794-6850\r
-1 DEST FTW\r
-1 DATE 28 Feb 2002\r
-1 CHAR ANSI\r
-1 SUBM @SUBM@\r
-1 FILE E:\Fam\Wolf1130.GED\r
-1 GEDC\r""")
-    assert len(g.as_list) == 14
+1 SOUR FTW\r""")
+    assert len(g.as_list) == 2
+
+def test_utf8_chars():
+    g = Gedcom(stream="""0 HEAD
+1 SOUR אינה""")
+    assert g.as_list[1].value == 'אינה'
+
